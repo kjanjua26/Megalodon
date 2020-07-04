c528e133d9de2f46bfbb8bf8749810aa4aff23a7
Kamran Janjua <kamranejaz98@gmail.com>
2020-07-04 23:27:54 +0500

    freq dist

--- Data-Analysis/dataset_distribution.py
+++ Data-Analysis/dataset_distribution.py
@@ -4,7 +4,7 @@ Some exploratory data analysis to check the data distribution.
 import glob, os
 import matplotlib.pyplot as plt
 from typing import List, Dict
-#plt.style('fivethirtyeight')
+plt.style.use('fivethirtyeight')
 
 basepath = "/Users/Janjua/Desktop/Projects/Megalodon/Finegrained-Images/data/"
 
@@ -13,5 +13,10 @@ species_count: Dict = {}
 print('Total species are: ', len(list_of_species))
 
 for ix, specie in enumerate(list_of_species):
-    
+    imgs = [x for x in glob.glob(specie + "/*.jpg".format(specie))]
+    name = specie.split('/')[-1].split(' Shark')[0]
+    species_count[name] = len(imgs)
 
+plt.xticks(fontsize=5, rotation=70)
+plt.bar(species_count.keys(), species_count.values())
+plt.show()
